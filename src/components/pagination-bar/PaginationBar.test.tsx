2f24d5e3cd5ab7aab2c175fbdc9a47ba3a5c6010
André Knob <knobandre@gmail.com>
2021-03-03 23:05:59 -0300

    added more test cases to pagination bar component

--- src/components/pagination-bar/PaginationBar.test.tsx
+++ src/components/pagination-bar/PaginationBar.test.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { render, screen } from '../../test-utils';
 import PaginationBar from './PaginationBar';
 
-function TestingComponent() {
+function TestingComponent({ searchParams = '' }) {
   return (
     <Router>
       <Switch>
@@ -13,7 +13,7 @@ function TestingComponent() {
           component={() => (
             <PaginationBar
               pagination={{ limit: 20, count: 20, total: 48554, offset: 0 }}
-              query={new URLSearchParams()}
+              query={new URLSearchParams(searchParams)}
             />
           )}
         />
@@ -25,7 +25,7 @@ function TestingComponent() {
 describe('PaginationBar', () => {
   describe('on rendering 2428 pages', () => {
     describe('at first page', () => {
-      test('renders the right buttons', async () => {
+      test('renders the correct buttons', async () => {
         render(<TestingComponent />);
 
         const linkElements = screen.getAllByRole('link');
@@ -38,6 +38,172 @@ describe('PaginationBar', () => {
           '4',
           '5',
           '...',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at third page', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=3" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '...',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at fifth page', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=5" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '...',
+          '4',
+          '5',
+          '6',
+          '...',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at sixth page', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=6" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '...',
+          '5',
+          '6',
+          '7',
+          '...',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at page 2425', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=2425" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '...',
+          '2424',
+          '2425',
+          '2426',
+          '2427',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at page 2427', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=2427" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '...',
+          '2425',
+          '2426',
+          '2427',
+          '2428',
+          null,
+          null,
+        ];
+
+        linkElements.forEach((element, index) => {
+          if (testingValues[index] != null) {
+            expect(element).toHaveTextContent(testingValues[index] ?? '');
+          }
+        });
+      });
+    });
+
+    describe('at last page', () => {
+      test('renders the correct buttons', async () => {
+        render(<TestingComponent searchParams="?page=2428" />);
+
+        const linkElements = screen.getAllByRole('link');
+
+        const testingValues = [
+          null,
+          null,
+          '1',
+          '...',
+          '2425',
+          '2426',
+          '2427',
           '2428',
           null,
           null,
